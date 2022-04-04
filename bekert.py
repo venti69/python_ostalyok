03429107783fa89cf8f04b49d1e55c5acb585330
venti69 <nagyvati.romeo@szbiszeged.hu>
2022-04-04 11:26:30 +0200

    negyedik kommit

---
+++ bekert.py
@@ -0,0 +1,6 @@
+class Beker:
+    def __init__(self):
+        self.szoveg = input('Kérek egy szót: ')
+
+    def kiir(self):
+        print(self.szoveg)
--- repoalapok.py
+++ repoalapok.py
@@ -1,2 +1,9 @@
 from os import system
+from beker import Beker
 system('cls')
+
+print('Hello world!')
+print.kiir()
+
+proba_2 = Beker()
+proba_2.kiir()
